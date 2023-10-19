bcab1b3820bd38f29e4b6701f1dc816d9ad62ae2
Leonardo <lramoslima97@gmail.com>
2023-10-18 21:41:28 -0300

    feat: :sparkles: create a new component list tile for user

---
+++ lib/components/user_tile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:userlist/models/user.dart';
+
+class UserTile extends StatelessWidget {
+  final User user;
+
+  const UserTile({super.key, required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    final CircleAvatar avatar;
+    if (user.avatarUrl == null || user.avatarUrl!.isEmpty) {
+      avatar = const CircleAvatar(child: Icon(Icons.person));
+    } else {
+      avatar = CircleAvatar(
+        backgroundImage: NetworkImage(user.avatarUrl.toString()),
+      );
+    }
+    return ListTile(
+      leading: avatar,
+      title: Text(user.name),
+      subtitle: user.email == null
+          ? const Text(
+              'Sem email',
+              style: TextStyle(color: Colors.red),
+            )
+          : Text(user.email.toString()),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              color: Theme.of(context).primaryColor,
+              icon: const Icon(Icons.edit),
+            ),
+            IconButton(
+              onPressed: () {},
+              color: Theme.of(context).primaryColor,
+              icon: const Icon(Icons.delete),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
