0d50f7564522e643da8c67ebb3d9ae46eae05d4d
Leonardo <lramoslima97@gmail.com>
2023-10-18 21:40:55 -0300

    feat: :sparkles: create mock data users

---
+++ lib/data/dummy_user.dart
@@ -0,0 +1,45 @@
+import 'package:userlist/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': User(
+      id: '1',
+      name: 'Fernando Guimaraes',
+      email: 'fernando@professor.com',
+      avatarUrl:
+          'https://www.ea.uniceub.br//Media/Foto?usrid=f86l0H_yuFXvzG1YI2eaSA2'),
+  '2': User(
+      id: '2',
+      name: 'Eloisa Masson',
+      email: 'eloisa@professor.com',
+      avatarUrl:
+          'https://www.ea.uniceub.br//Media/Foto?usrid=LDfRX-4iKfHI0NDRXo9ddA2'),
+  '3': User(
+      id: '3',
+      name: 'Leonardo Suarez',
+      email: 'leonardosuarez@professor.com',
+      avatarUrl:
+          'https://www.ea.uniceub.br//Media/Foto?usrid=fD-KZ-yCQcBDoBZdOOktew2'),
+  '4': User(
+      id: '4',
+      name: 'Deusdeth Mariano',
+      avatarUrl:
+          'https://www.ea.uniceub.br//Media/Foto?usrid=_7dAnKRBRGT2axdEh6daUA2'),
+  '5': User(
+      id: '5',
+      name: 'Roberto Paldes',
+      email: 'robertopaldes@professor.com',
+      avatarUrl:
+          'https://www.ea.uniceub.br//Media/Foto?usrid=dmzfsXVi6V5cP1etCBuq1g2'),
+  '6': User(
+      id: '6',
+      name: 'Antonio Alves',
+      email: 'antonioalves@professor.com',
+      avatarUrl:
+          'https://www.ea.uniceub.br//Media/Foto?usrid=AdsIjvPAcJa4h9rtsIOWpA2'),
+  '7': User(
+      id: '7',
+      name: 'Rogério Conceição',
+      email: 'terrivel@professor.com',
+      avatarUrl:
+          'https://www.ea.uniceub.br//Media/Foto?usrid=EyS1hBi07zbSa00w5-tm-w2'),
+};
