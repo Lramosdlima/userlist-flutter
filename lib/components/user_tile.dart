dd21950806b1d98ea984197f45ffb7df255a2b17
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-10-30 21:44:13 -0300

    refactor: :recycle: improvement decoupling code and add route

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:userlist/models/user.dart';
+import 'package:userlist/routes/app_routes.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
@@ -9,27 +10,24 @@ class UserTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final CircleAvatar avatar;
-    if (user.avatarUrl == null || user.avatarUrl!.isEmpty) {
-      avatar = const CircleAvatar(child: Icon(Icons.person));
-    } else {
-      avatar =
-          CircleAvatar(backgroundImage: Image.asset(user.avatarUrl!).image);
-    }
+
+    avatar = avatarImage(user.avatarUrl);
+
     return ListTile(
       leading: avatar,
       title: Text(user.name),
-      subtitle: user.email == null
-          ? const Text(
-              'Email não identificado',
-              style: TextStyle(color: Colors.red),
-            )
-          : Text(user.email.toString()),
+      subtitle: emailText(user.email),
       trailing: SizedBox(
         width: 100,
         child: Row(
           children: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.USER_FORM,
+                  arguments: user,
+                );
+              },
               color: Theme.of(context).primaryColor,
               icon: const Icon(Icons.edit),
             ),
@@ -42,5 +40,26 @@ class UserTile extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+avatarImage(avatarUrl) {
+  if (avatarUrl == null || avatarUrl!.isEmpty) {
+    return const CircleAvatar(child: Icon(Icons.person));
+  } else if (avatarUrl.startsWith('http')) {
+    return CircleAvatar(backgroundImage: NetworkImage(avatarUrl));
+  } else {
+    return CircleAvatar(backgroundImage: Image.asset(avatarUrl!).image);
+  }
+}
+
+emailText(email) {
+  if (email == null) {
+    return const Text(
+      'Email não identificado',
+      style: TextStyle(color: Colors.red),
+    );
+  } else {
+    return Text(email.toString());
   }
 }
